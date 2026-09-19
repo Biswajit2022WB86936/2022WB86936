427fb6caec3c833915f7828360c1a9eb7d56b8bd
Biswajit2022WB86936 <2022wb86936@wilp.bits-pilani.ac.in>
2026-09-19 18:03:17 +0530

    Modify source code for continuous integration

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -3,5 +3,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("DevOps Lab - Continuous Integration");
 		System.out.println("Maven Build Successful");
+		System.out.println("Source code modified successfully");
 	}
 }
